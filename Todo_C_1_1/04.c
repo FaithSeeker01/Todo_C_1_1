297894cd5864b699f24c11fe420f4077ec9d14ec
FaithSeeker01 <113914475+FaithSeeker01@users.noreply.github.com>
2022-10-02 11:26:56 +0800

    Update 04.c

--- Todo_C_1_1/04.c
+++ Todo_C_1_1/04.c
@@ -1,10 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
-int main04()
+int main()
 {
 	int a, b, c, d, t;
-	//Ëã·¨£º½»»»Êý×Ö
+	//Ã‹Ã£Â·Â¨Â£ÂºÂ½Â»Â»Â»ÃŠÃ½Ã—Ã–
 	scanf("%d %d %d %d", &a, &b, &c, &d);
 	printf("before:A:%d B:%d C:%d D:%d\n", a, b, c, d);
 	t = a; a = b; b = t;
